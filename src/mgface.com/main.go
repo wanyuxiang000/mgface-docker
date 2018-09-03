ee350b62039dcab9072a7c05508bb1970082cecd
Yuxiang Wan <15622535353@163.com>
2018-09-03 13:42:10 +0800

    init

--- src/mgface.com/main.go
+++ src/mgface.com/main.go
@@ -4,6 +4,8 @@ import (
 	logs "github.com/Sirupsen/logrus"
 	"os"
 	"os/exec"
+	"os/user"
+	"strconv"
 	"syscall"
 )
 
@@ -13,7 +15,14 @@ func main() {
 		Cloneflags: syscall.CLONE_NEWUTS | syscall.CLONE_NEWPID | syscall.CLONE_NEWIPC |
 			syscall.CLONE_NEWNS | syscall.CLONE_NEWUSER | syscall.CLONE_NEWNET,
 	}
-	cmd.SysProcAttr.Credential = &syscall.Credential{Uid: uint32(0), Gid: uint32(0)}
+	user,err:=user.Lookup("nobody")
+	if err == nil {
+		logs.Errorf("uid=%s,gid=%s",user.Uid,user.Gid)
+	}
+	uid,_:=strconv.Atoi(user.Uid)
+	gid,_:=strconv.Atoi(user.Gid)
+
+	cmd.SysProcAttr.Credential = &syscall.Credential{Uid: uint32(uid), Gid: uint32(gid)}
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
