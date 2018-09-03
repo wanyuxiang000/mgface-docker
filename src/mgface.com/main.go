730d60a643d67e42c0b4b12e61a6fc8db676e732
Yuxiang Wan <15622535353@163.com>
2018-09-03 09:51:02 +0800

    init

--- src/mgface.com/main.go
+++ src/mgface.com/main.go
@@ -13,7 +13,7 @@ func main() {
 		Cloneflags: syscall.CLONE_NEWUTS | syscall.CLONE_NEWPID | syscall.CLONE_NEWIPC |
 			syscall.CLONE_NEWNS | syscall.CLONE_NEWUSER | syscall.CLONE_NEWNET,
 	}
-	cmd.SysProcAttr.Credential = &syscall.Credential{Uid: uint32(1), Gid: uint32(1)}
+	cmd.SysProcAttr.Credential = &syscall.Credential{Uid: uint32(0), Gid: uint32(0)}
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
