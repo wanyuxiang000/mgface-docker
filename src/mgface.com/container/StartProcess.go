907f64c4c8b018a1b75d2c6497844a84eaf5369a
Yuxiang Wan <15622535353@163.com>
2018-09-04 11:35:52 +0800

    容器创建

--- src/mgface.com/container/StartProcess.go
+++ src/mgface.com/container/StartProcess.go
@@ -14,6 +14,9 @@ func RunContainerInitProcess(command string, _ []string) error {
 	defaultMountFlags := syscall.MS_NOEXEC | syscall.MS_NOSUID | syscall.MS_NODEV
 	syscall.Mount("proc", "/proc", "proc", uintptr(defaultMountFlags), "")
 	argv := []string{command}
+	//这个方法是调用内核的int execve(const char *filename,char * const argv[],char *const envp[])
+	//它的作用是执行当前filename对应的程序。它会覆盖当前进程的镜像、数据和堆械等信息，包括 PID.这些都会被将要运行的进程覆盖掉。
+	//也就是说，调用这个方法，将用户指定的进程运行起来，把最初的 init 进程给替换掉，这样当进入到容器内部的时候，就会发现容器内的第一个程序就是我们指定的进程了
 	if err := syscall.Exec(command, argv, os.Environ()); err != nil {
 		logrus.Errorf(err.Error())
 	}
