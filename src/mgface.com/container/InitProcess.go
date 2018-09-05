b45057b4433ecc5930d4483c099a13aec3784605
Yuxiang Wan <15622535353@163.com>
2018-09-05 13:51:08 +0800

    添加pivotRoot切换

--- src/mgface.com/container/InitProcess.go
+++ src/mgface.com/container/InitProcess.go
@@ -1,68 +1,79 @@
 package container
 
 import (
+	"fmt"
 	"github.com/Sirupsen/logrus"
-	"mgface.com/subsystem"
+	"io/ioutil"
 	"os"
 	"os/exec"
+	"path/filepath"
 	"strings"
 	"syscall"
 )
 
-func NewParentProcess(tty bool) (*exec.Cmd, *os.File) {
-	r, w, _ := os.Pipe()
-
-	args := []string{"init"}
-	cmd := exec.Command("/proc/self/exe", args...)
-	cmd.SysProcAttr = &syscall.SysProcAttr{
-		Cloneflags: syscall.CLONE_NEWUTS | syscall.CLONE_NEWPID | syscall.CLONE_NEWNS | syscall.CLONE_NEWNET | syscall.CLONE_NEWUSER | syscall.CLONE_NEWIPC,
+func RunContainerInitProcess() error {
+	cmdArray := readUserCommand()
+	logrus.Infof("接收到的命令:%s", cmdArray)
+	if cmdArray == nil || len(cmdArray) == 0 {
+		return fmt.Errorf("Run container get user command error,cmdArray is nil")
 	}
 
-	//相当于 cmd 进程认为自己是以 root 执行的，但其实最终的操作受制于 宿主机0(root)这个用户
+	setUpMount()
 
-	//设置该进程在新命名空间中以root用户执行。而这个root用户则是映射到host上用户id为 0(root)、组 id 为 0(root) 的用户
-	cmd.SysProcAttr.Credential = &syscall.Credential{
-		Uid: 0,
-		Gid: 0,
+	path, err := exec.LookPath(cmdArray[0])
+
+	if err != nil {
+		logrus.Errorf("Exec loop path error %v", err)
+		return err
 	}
-
-	cmd.SysProcAttr.UidMappings = []syscall.SysProcIDMap{{ContainerID: 0, HostID: 0, Size: 1}}
-	cmd.SysProcAttr.GidMappings = []syscall.SysProcIDMap{{ContainerID: 0, HostID: 0, Size: 1}}
-	if tty {
-		cmd.Stdin = os.Stdin
-		cmd.Stdout = os.Stdout
-		cmd.Stderr = os.Stderr
+	logrus.Infof("Find path %s", path)
+	//MS_NOEXEC 本文件系统不允许运行其他程序
+	//MS_NOSUID 不允许setuserId和setGroupId
+	//MS NODEV 这个参数是自 从 Linux 2.4 以来，所有 mount 的系统都会默认设定的参数
+	//defaultMountFlags := syscall.MS_NOEXEC | syscall.MS_NOSUID | syscall.MS_NODEV
+	//syscall.Mount("proc", "/proc", "proc", uintptr(defaultMountFlags), "")
+	//argv := []string{command}
+	//这个方法是调用内核的int execve(const char *filename,char * const argv[],char *const envp[])
+	//它的作用是执行当前filename对应的程序。它会覆盖当前进程的镜像、数据和堆械等信息，包括 PID.这些都会被将要运行的进程覆盖掉。
+	//也就是说，调用这个方法，将用户指定的进程运行起来，把最初的 init 进程给替换掉，这样当进入到容器内部的时候，就会发现容器内的第一个程序就是我们指定的进程了
+	logrus.Infof("命令行:%s", cmdArray[0:])
+	if err := syscall.Exec(path, cmdArray[0:], os.Environ()); err != nil {
+		logrus.Errorf(err.Error())
 	}
-	//会外带着这个文件句柄去创建子进程
-	//因为 1 个进程默认会有 3 个文件描述符，分别是标准输入、标准输出、标准错误。这3个
-	//是子进程一创建的时候就会默认带着的，那么外带的这个文件描述符理所当然地就成为了第4个
-	cmd.ExtraFiles = []*os.File{r}
-
-	//设置cmd的目录
-	cmd.Dir = "/root/busybox"
-	return cmd, w
+	return nil
 }
 
-func Run(tty bool, command []string, res *subsystem.ResouceConfig) {
-	cmd, writePipe := NewParentProcess(tty)
-	if err := cmd.Start(); err != nil {
-		logrus.Fatal("发生错误:%s", err)
-	}
-	manager := subsystem.NewCgroupManager("mgface-cgroup1")
-	defer manager.Destory()
-	//设置资源限制
-	manager.Set(res)
-	//将容器进程加入到各个subsystem挂载对于的cgroup
-	manager.Apply(cmd.Process.Pid)
-
-	sendInitCommand(command, writePipe)
-	cmd.Wait()
-	os.Exit(-1)
+func readUserCommand() []string {
+	pipe := os.NewFile(uintptr(3), "pipe")
+	msg, _ := ioutil.ReadAll(pipe)
+	msgstr := string(msg)
+	return strings.Split(msgstr, " ")
 }
 
-func sendInitCommand(comArray []string, writePipe *os.File) {
-	command := strings.Join(comArray, " ")
-	logrus.Infof("所有的命令:%s", command)
-	writePipe.WriteString(command)
-	writePipe.Close()
+func pivotRoot(root string) error {
+	if err := syscall.Mount(root, root, "bind", syscall.MS_BIND|syscall.MS_REC, ""); err != nil {
+		return fmt.Errorf("挂载rootfs给自己发生错误:%v", err)
+	}
+	pivotDir := filepath.Join(root, ".pivot_root")
+	os.Mkdir(pivotDir, 0777)
+	//pivotRoot把当前进程的root系统移动到putold文件夹，然后让new_root成为新root的文件系统
+	syscall.PivotRoot(root, pivotDir)
+
+	syscall.Chdir("/")
+
+	pivotDir = filepath.Join("/", ".pivot_root")
+
+	syscall.Unmount(pivotDir, syscall.MNT_DETACH)
+
+	return os.Remove(pivotDir)
+
+}
+
+func setUpMount() {
+	pwd, _ := os.Getwd()
+	logrus.Infof("当前的location: %s", pwd)
+	pivotRoot(pwd)
+	defaultMountFlags := syscall.MS_NOEXEC | syscall.MS_NOSUID | syscall.MS_NODEV
+	syscall.Mount("proc", "/proc", "proc", uintptr(defaultMountFlags), "")
+	syscall.Mount("tmpfs", "/dev", "tempfs", syscall.MS_NOSUID|syscall.MS_STRICTATIME, "mode=755")
 }
---
+++ src/mgface.com/container/runProcess.go
@@ -0,0 +1,68 @@
+package container
+
+import (
+	"github.com/Sirupsen/logrus"
+	"mgface.com/subsystem"
+	"os"
+	"os/exec"
+	"strings"
+	"syscall"
+)
+
+func NewParentProcess(tty bool) (*exec.Cmd, *os.File) {
+	r, w, _ := os.Pipe()
+
+	args := []string{"init"}
+	cmd := exec.Command("/proc/self/exe", args...)
+	cmd.SysProcAttr = &syscall.SysProcAttr{
+		Cloneflags: syscall.CLONE_NEWUTS | syscall.CLONE_NEWPID | syscall.CLONE_NEWNS | syscall.CLONE_NEWNET | syscall.CLONE_NEWUSER | syscall.CLONE_NEWIPC,
+	}
+
+	//相当于 cmd 进程认为自己是以 root 执行的，但其实最终的操作受制于 宿主机0(root)这个用户
+
+	//设置该进程在新命名空间中以root用户执行。而这个root用户则是映射到host上用户id为 0(root)、组 id 为 0(root) 的用户
+	cmd.SysProcAttr.Credential = &syscall.Credential{
+		Uid: 0,
+		Gid: 0,
+	}
+
+	cmd.SysProcAttr.UidMappings = []syscall.SysProcIDMap{{ContainerID: 0, HostID: 0, Size: 1}}
+	cmd.SysProcAttr.GidMappings = []syscall.SysProcIDMap{{ContainerID: 0, HostID: 0, Size: 1}}
+	if tty {
+		cmd.Stdin = os.Stdin
+		cmd.Stdout = os.Stdout
+		cmd.Stderr = os.Stderr
+	}
+	//会外带着这个文件句柄去创建子进程
+	//因为 1 个进程默认会有 3 个文件描述符，分别是标准输入、标准输出、标准错误。这3个
+	//是子进程一创建的时候就会默认带着的，那么外带的这个文件描述符理所当然地就成为了第4个
+	cmd.ExtraFiles = []*os.File{r}
+
+	//设置cmd的目录
+	cmd.Dir = "/root/busybox"
+	return cmd, w
+}
+
+func Run(tty bool, command []string, res *subsystem.ResouceConfig) {
+	cmd, writePipe := NewParentProcess(tty)
+	if err := cmd.Start(); err != nil {
+		logrus.Fatal("发生错误:%s", err)
+	}
+	manager := subsystem.NewCgroupManager("mgface-cgroup1")
+	defer manager.Destory()
+	//设置资源限制
+	manager.Set(res)
+	//将容器进程加入到各个subsystem挂载对于的cgroup
+	manager.Apply(cmd.Process.Pid)
+
+	sendInitCommand(command, writePipe)
+	cmd.Wait()
+	os.Exit(-1)
+}
+
+func sendInitCommand(comArray []string, writePipe *os.File) {
+	command := strings.Join(comArray, " ")
+	logrus.Infof("所有的命令:%s", command)
+	writePipe.WriteString(command)
+	writePipe.Close()
+}
