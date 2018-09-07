328a694c4a89dd75680db630b9ead8a4747afbc7
Yuxiang Wan <15622535353@163.com>
2018-09-07 11:56:16 +0800

    添加pivotRoot切换

--- src/mgface.com/container/runProcess.go
+++ src/mgface.com/container/runProcess.go
@@ -12,22 +12,21 @@ import (
 func NewParentProcess(tty bool) (*exec.Cmd, *os.File) {
 	r, w, _ := os.Pipe()
 
-	args := []string{"init"}
-	cmd := exec.Command("/proc/self/exe", args...)
+	cmd := exec.Command("/proc/self/exe", "init")
 	cmd.SysProcAttr = &syscall.SysProcAttr{
-		Cloneflags: syscall.CLONE_NEWUTS | syscall.CLONE_NEWPID | syscall.CLONE_NEWNS | syscall.CLONE_NEWNET | syscall.CLONE_NEWUSER | syscall.CLONE_NEWIPC,
+		Cloneflags: syscall.CLONE_NEWUTS | syscall.CLONE_NEWPID | syscall.CLONE_NEWNS | syscall.CLONE_NEWNET | syscall.CLONE_NEWIPC, //| syscall.CLONE_NEWUSER
 	}
 
 	//相当于 cmd 进程认为自己是以 root 执行的，但其实最终的操作受制于 宿主机0(root)这个用户
 
 	//设置该进程在新命名空间中以root用户执行。而这个root用户则是映射到host上用户id为 0(root)、组 id 为 0(root) 的用户
-	cmd.SysProcAttr.Credential = &syscall.Credential{
-		Uid: 0,
-		Gid: 0,
-	}
-
-	cmd.SysProcAttr.UidMappings = []syscall.SysProcIDMap{{ContainerID: 0, HostID: 0, Size: 1}}
-	cmd.SysProcAttr.GidMappings = []syscall.SysProcIDMap{{ContainerID: 0, HostID: 0, Size: 1}}
+	//cmd.SysProcAttr.Credential = &syscall.Credential{
+	//	Uid: 0,
+	//	Gid: 0,
+	//}
+	//
+	//cmd.SysProcAttr.UidMappings = []syscall.SysProcIDMap{{ContainerID: 0, HostID: 0, Size: 1}}
+	//cmd.SysProcAttr.GidMappings = []syscall.SysProcIDMap{{ContainerID: 0, HostID: 0, Size: 1}}
 	if tty {
 		cmd.Stdin = os.Stdin
 		cmd.Stdout = os.Stdout
