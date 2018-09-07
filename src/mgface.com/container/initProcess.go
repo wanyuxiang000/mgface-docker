da528ba84ec339ffc41aba6a0ce2adab516dc77a
Yuxiang Wan <15622535353@163.com>
2018-09-07 13:42:11 +0800

    屏蔽用户信息

--- src/mgface.com/container/initProcess.go
+++ src/mgface.com/container/initProcess.go
@@ -51,27 +51,27 @@ func readUserCommand() []string {
 }
 
 func pivotRoot(root string) error {
-	if err := syscall.Mount(root, root, "", syscall.MS_BIND|syscall.MS_REC, ""); err != nil {
+	if err := syscall.Mount(root, root, "bind", syscall.MS_BIND|syscall.MS_REC, ""); err != nil {
 		return fmt.Errorf("挂载rootfs给自己发生错误:%v", err)
 	}
 	pivotDir := filepath.Join(root, ".pivot_root")
-	fmt.Println("pivotDir->",pivotDir)
-	if err:=os.MkdirAll(pivotDir, 0700);err!=nil{
+	fmt.Println("pivotDir->", pivotDir)
+	if err := os.Mkdir(pivotDir, 0777); err != nil {
 		return err
 	}
 	//pivotRoot把当前进程的root系统移动到putold文件夹，然后让new_root成为新root的文件系统
-	if err:=syscall.PivotRoot(root, pivotDir);err!=nil{
-		return fmt.Errorf("pivot_root %v",err)
+	if err := syscall.PivotRoot(root, pivotDir); err != nil {
+		return fmt.Errorf("pivot_root %v", err)
 	}
 
-	if err:=syscall.Chdir("/");err!=nil{
-		return fmt.Errorf("chdir / %v",err)
+	if err := syscall.Chdir("/"); err != nil {
+		return fmt.Errorf("chdir / %v", err)
 	}
 
 	pivotDir = filepath.Join("/", ".pivot_root")
 
-	if err:=syscall.Unmount(pivotDir, syscall.MNT_DETACH);err!=nil{
-		return fmt.Errorf("umount pivot_root dir %v",err)
+	if err := syscall.Unmount(pivotDir, syscall.MNT_DETACH); err != nil {
+		return fmt.Errorf("umount pivot_root dir %v", err)
 	}
 
 	return os.Remove(pivotDir)
@@ -81,9 +81,13 @@ func pivotRoot(root string) error {
 func setUpMount() {
 	pwd, _ := os.Getwd()
 	logrus.Infof("当前的location: %s", pwd)
-	err:=pivotRoot(pwd)
-	logrus.Infof("pivotRoot切换->%v",err)
+	err := pivotRoot(pwd)
+	logrus.Infof("pivotRoot切换->%v", err)
 	defaultMountFlags := syscall.MS_NOEXEC | syscall.MS_NOSUID | syscall.MS_NODEV
-	syscall.Mount("proc", "/proc", "proc", uintptr(defaultMountFlags), "")
-	syscall.Mount("tmpfs", "/dev", "tempfs", syscall.MS_NOSUID|syscall.MS_STRICTATIME, "mode=755")
+	if err := syscall.Mount("proc", "/proc", "proc", uintptr(defaultMountFlags), ""); err != nil {
+		fmt.Println("mount proc 发生错误:", err)
+	}
+	if err := syscall.Mount("tmpfs", "/dev", "tempfs", syscall.MS_NOSUID|syscall.MS_STRICTATIME, "mode=755"); err != nil {
+		fmt.Println("mount tmpfs 发生错误:", err)
+	}
 }
