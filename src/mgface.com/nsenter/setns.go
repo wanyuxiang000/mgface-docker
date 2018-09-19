49f9e4086aec37961a4d686bcf6649caeddb62e5
Yuxiang Wan <15622535353@163.com>
2018-09-19 11:05:32 +0800

    创建exec参数-修改cgo的代码

--- src/mgface.com/nsenter/setns.go
+++ src/mgface.com/nsenter/setns.go
@@ -9,16 +9,12 @@ package nsenter
 #include <stdlib.h>
 #include <string.h>
 #include <fcntl.h>
-//这里的__attribute__((constructor))指的是， 一旦这个包被引用，那么这个函数就会被自动执行
-//类似于构造函数，会在程序一启动的时候运行
 __attribute__((constructor)) void enter_namespace(void) {
 	char *mydocker_pid;
-	//从环境变量中获取需要进入的PID
 	mydocker_pid = getenv("mydocker_pid");
 	if (mydocker_pid) {
 		fprintf(stdout, "got mydocker_pid=%s\n", mydocker_pid);
 	} else {
-	    //这里如果没有指定PID就不需要向下执行,直接退出
 		fprintf(stdout, "missing mydocker_pid env skip nsenter");
 		return;
 	}
@@ -33,11 +29,9 @@ __attribute__((constructor)) void enter_namespace(void) {
 	int i;
 	char nspath[1024];
 	char *namespaces[] = { "ipc", "uts", "net", "pid", "mnt" };
-
 	for (i=0; i<5; i++) {
 		sprintf(nspath, "/proc/%s/ns/%s", mydocker_pid, namespaces[i]);
 		int fd = open(nspath, O_RDONLY);
-		//这里才真正调用 setns 系统调用进入对应的 Namespace
 		if (setns(fd, 0) == -1) {
 			fprintf(stderr, "setns on %s namespace failed: %s\n", namespaces[i], strerror(errno));
 		} else {
@@ -45,7 +39,6 @@ __attribute__((constructor)) void enter_namespace(void) {
 		}
 		close(fd);
 	}
-	//在进入的 Namespace 中执行指定的命令
 	int res = system(mydocker_cmd);
 	exit(0);
 	return;
