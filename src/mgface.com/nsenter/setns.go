736aedb5865d74248473948ec4ee636fc5a3b6da
Yuxiang Wan <15622535353@163.com>
2018-09-19 13:16:18 +0800

    创建exec参数-修改cgo的代码

--- src/mgface.com/nsenter/setns.go
+++ src/mgface.com/nsenter/setns.go
@@ -11,20 +11,20 @@ package nsenter
 __attribute__((constructor)) void enter_namespace(void) {
 	printf("这个包一旦被引用,它就会在所有的go运行的环境启动之前执行,这样就避免了Go多线程导致的无法进入mnt Namespace的问题.这段程序执行完毕后,Go程序才会执行.\n");
 	char *mydocker_pid;
-	mydocker_pid = getenv("mydocker_pid");
+	mydocker_pid = getenv("mgfacedocker_pid");
 	 printf("Cgo进行exec实现PID:%s\n", mydocker_pid);
 	if (mydocker_pid) {
-		printf("got mydocker_pid=%s\n", mydocker_pid);
+		printf("获取mgfacedocker_pid=%s\n", mydocker_pid);
 	} else {
-		printf("missing mydocker_pid env skip nsenter\n");
+		printf("没有获取到mgfacedocker_pid环境跳过nsenter\n");
 		return;
 	}
 	char *mydocker_cmd;
-	mydocker_cmd = getenv("mydocker_cmd");
+	mydocker_cmd = getenv("mgfacedocker_cmd");
 	if (mydocker_cmd) {
-		fprintf(stdout, "got mydocker_cmd=%s\n", mydocker_cmd);
+		fprintf(stdout, "获取mgfacedocker_cmd=%s\n", mydocker_cmd);
 	} else {
-		fprintf(stdout, "missing mydocker_cmd env skip nsenter");
+		fprintf(stdout, "没有获取到mgfacedocker_cmd环境跳过nsenter");
 		return;
 	}
 	int i;
@@ -34,9 +34,9 @@ __attribute__((constructor)) void enter_namespace(void) {
 		sprintf(nspath, "/proc/%s/ns/%s", mydocker_pid, namespaces[i]);
 		int fd = open(nspath, O_RDONLY);
 		if (setns(fd, 0) == -1) {
-			fprintf(stderr, "setns on %s namespace failed: %s\n", namespaces[i], strerror(errno));
+			fprintf(stderr, "setns on %s namespace 失败: %s\n", namespaces[i], strerror(errno));
 		} else {
-			fprintf(stdout, "setns on %s namespace succeeded\n", namespaces[i]);
+			fprintf(stdout, "setns on %s namespace 成功\n", namespaces[i]);
 		}
 		close(fd);
 	}
