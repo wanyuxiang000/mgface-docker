dd6fdaa1ff1f80b99178c59742138daf5cae153c
Yuxiang Wan <15622535353@163.com>
2018-09-26 17:34:36 +0800

    修复网络空间问题

--- src/mgface.com/command/runCommand.go
+++ src/mgface.com/command/runCommand.go
@@ -85,13 +85,13 @@ var RunCommand = cli.Command{
 		}
 		logrus.Infof("入参tty:%t,命令:%s", tty, cmdArray)
 
-		logrus.Infof("0000.........")
+		logrus.Infof("1.........")
 		//获得volume配置
 		volume := ctx.String("v")
-
+		logrus.Infof("2.........")
 		//获得环境变量
 		envs := ctx.StringSlice("e")
-
+		logrus.Infof("3.........")
 		//连接的网络
 		network := ctx.String("net")
 		//获取端口映射
